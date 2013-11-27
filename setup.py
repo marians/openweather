d46916efbae14ee8e2bd45b9fbc14613568eba52
Marian Steinbach <marian@sendung.de>
2013-11-27 10:05:58 +0100

    Better README on PyPi!
    Thanks to https://coderwall.com/p/qawuyq

--- setup.py
+++ setup.py
@@ -2,9 +2,16 @@
 
 from setuptools import setup
 
+try:
+    import pypandoc
+    description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+    description = ''
+
 setup(name='openweather',
       version='0.7',
       description='OpenWeatherMap.org API wrapper',
+      long_description=description,
       author='Marian Steinbach',
       author_email='marian@sendung.de',
       url='http://github.com/marians/openweather',
