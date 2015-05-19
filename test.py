0ae39738e654e13a74f07a09e3f2bed045a195bd
marians <marian@giantswarm.io>
2015-05-19 21:44:59 +0200

    Testcase updates for API 2.5

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ import os
 class TestSequenceFunctions(unittest.TestCase):
 
     def setUp(self):
-        self.ow_nocache = openweather.OpenWeather(cache=False)
+        self.ow_nocache = openweather.OpenWeather(cache=False, verbose=True)
         self.ow = openweather.OpenWeather(cache="openweather-unnittest-cache.db")
         self.station_ids = [4885]  # Airport Cologne/Bonn
 
@@ -20,21 +20,21 @@ class TestSequenceFunctions(unittest.TestCase):
         os.remove("openweather-unnittest-cache.db")
 
     def test_find_stations_1(self):
-        stations = self.ow_nocache.find_stations_near(7.0, 50.0, 100)
-        self.assertIs(type(stations), dict)
-        self.assertTrue("list" in stations)
-        self.assertIs(type(stations["list"]), list)
+        stations = self.ow_nocache.find_stations_near(7.0, 50.0, 20)
+        self.assertIs(type(stations), list)
+        self.assertTrue("station" in stations[0])
+        self.assertTrue("distance" in stations[0])
+        self.assertTrue("last" in stations[0])
 
     def test_get_weather_1(self):
         weather = self.ow_nocache.get_weather(self.station_ids[0])
         self.assertIs(type(weather), dict)
-        self.assertTrue("id" in weather)
         self.assertTrue("main" in weather)
-        self.assertTrue("name" in weather)
-        self.assertTrue("coord" in weather)
-        self.assertTrue("station" in weather)
-        self.assertTrue("calc" in weather)
         self.assertTrue("wind" in weather)
+        self.assertTrue("visibility" in weather)
+        self.assertTrue("calc" in weather)
+        self.assertTrue("clouds" in weather)
+        self.assertTrue("dt" in weather)
 
     def test_get_historic_weather_nocache_1(self):
         start = datetime.utcnow() - timedelta(days=5)
@@ -75,13 +75,20 @@ class TestSequenceFunctions(unittest.TestCase):
         self.assertTrue(self.ow.cache.fetchone()[0] > 0)
 
     def test_get_historic_weather_nocache_2(self):
-        fromd = datetime(2013, 8, 1, 10)
-        tod = datetime(2013, 8, 1, 11)
+        fromd = datetime(2015, 5, 1, 10)
+        tod = datetime(2014, 5, 1, 11)
         weather = self.ow_nocache.get_historic_weather(
             station_id=4885,
             from_date=fromd,
             to_date=tod)
         self.assertIs(type(weather), list)
+        self.assertTrue("calc" in weather[0])
+        self.assertTrue("temp" in weather[0])
+        self.assertTrue("humidity" in weather[0])
+        self.assertTrue("pressure" in weather[0])
+        self.assertTrue("main" in weather[0])
+        self.assertTrue("dt" in weather[0])
+        self.assertTrue("wind" in weather[0])
 
     def test_get_historic_weather_cache_2(self):
         """
